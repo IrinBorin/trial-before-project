381e85f031b43489aebf081b1b0a195f90451938
IB <holt.irina@gmail.com>
2024-07-05 19:41:31 +0200

    entry

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, this is me!");
         System.out.println("This is Ira.");
+        System.out.println("This is Pavel!");
     }
 }
